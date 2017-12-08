37a7879762b75c6bf6fd4412cdaad25ae8829a0e
Kevin Adistambha <kevinadi@gmail.com>
2017-12-08 15:11:19 +1100

    CR tool now uses environment variables

--- functions/cr.fish
+++ functions/cr.fish
@@ -1,3 +1,11 @@
 function cr
-	python ~/Development/github/kernel-tools/codereview/upload.py --email kevin.adistambha@10gen.com --jira_user kevin.adistambha -m $argv
+	if [ (count $argv) = 0 ]
+        echo First commit for CR:
+        echo cr \"TSPROJ-XXXX short description here\" --rev GIT_COMMIT_HASH_OF_THE_COMMIT_JUST_BEFORE_YOUR_WORK
+        echo
+        echo Revise a CR:
+        echo cr \"short description of additional changes\" --rev GIT_COMMIT_HASH_OF_THE_COMMIT_JUST_BEFORE_YOUR_WORK -i 12345678
+    else
+	    python ~/Development/github/kernel-tools/codereview/upload.py --email $CR_EMAIL --jira_user $CR_JIRA -m $argv
+    end
 end
