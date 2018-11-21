5cd45c1349da930ed58222f25b110a6e590c42b4
Kevin Adistambha <kevinadi@gmail.com>
2018-11-21 16:37:38 +1100

    Modified emacsq

--- functions/emacsq.fish
+++ functions/emacsq.fish
@@ -1,4 +1,5 @@
-# Defined in /var/folders/rk/17j8535147q2c3g99p4009mw0000gp/T//fish.PbVvGl/emacsq.fish @ line 2
+# Defined in /var/folders/rk/17j8535147q2c3g99p4009mw0000gp/T//fish.FnqNHA/emacsq.fish @ line 2
 function emacsq
-	emacs -q --eval "(progn (load-theme 'wombat) (package-initialize) (windmove-default-keybindings))" $argv
+	# for bash: alias emacsq="emacs -q --eval \"(progn (load-theme 'wombat) (package-initialize) (windmove-default-keybindings))\" $@"
+    emacs -q --eval "(progn (load-theme 'wombat) (package-initialize) (windmove-default-keybindings) (osx-clipboard-mode) (which-key-mode) (abbrev-mode) (setq confirm-kill-emacs 'y-or-n-p))" $argv
 end
