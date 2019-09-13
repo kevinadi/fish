cf73017d919e0101233cdf66bf04f82c19cae0bd
Kevin Adistambha <kevinadi@gmail.com>
2019-09-13 10:32:43 +1000

    change prompt

--- functions/emacsq.fish
+++ functions/emacsq.fish
@@ -1,5 +1,5 @@
-# Defined in /var/folders/rk/17j8535147q2c3g99p4009mw0000gp/T//fish.FnqNHA/emacsq.fish @ line 2
+# Defined in /var/folders/rk/17j8535147q2c3g99p4009mw0000gp/T//fish.7la6Lv/emacsq.fish @ line 2
 function emacsq
-	# for bash: alias emacsq="emacs -q --eval \"(progn (load-theme 'wombat) (package-initialize) (windmove-default-keybindings))\" $@"
-    emacs -q --eval "(progn (load-theme 'wombat) (package-initialize) (windmove-default-keybindings) (osx-clipboard-mode) (which-key-mode) (abbrev-mode) (setq confirm-kill-emacs 'y-or-n-p))" $argv
+	# for bash: alias emacsq="emacs -q --eval \"(progn (load-theme 'wombat) (package-initialize) (windmove-default-keybindings) (abbrev-mode) (setq confirm-kill-emacs 'y-or-n-p) (electric-pair-mode) (setq make-backup-files nil) )\" $@"
+    emacs -q --eval "(progn (load-theme 'wombat) (package-initialize) (windmove-default-keybindings) (osx-clipboard-mode) (which-key-mode) (abbrev-mode) (setq confirm-kill-emacs 'y-or-n-p) (electric-pair-mode) (setq make-backup-files nil) )" $argv
 end
